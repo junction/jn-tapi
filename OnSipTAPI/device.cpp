581f8ba45db7989793931fe01ef232c31fbb0301
unknown <Ron@.(none)>
2009-07-17 13:50:33 -0500

    renamed doc files to be "OnSIP" ...

--- OnSipTAPI/device.cpp
+++ OnSipTAPI/device.cpp
@@ -244,14 +244,14 @@ long COnSipDevice::DRV_MakeCall(const COnSipLine* /*pLine*/, const TString& strD
 bool COnSipDevice::OpenDevice (CTSPIConnection* pConn)
 {
 	// TEST RSLBAD TODO remove, test to see if we can write to a file from TAPI access
-	FileIO f;
-	TCHAR szPath[MAX_PATH];
-	SHGetFolderPath( NULL, CSIDL_COMMON_APPDATA, NULL, 0, szPath );
-	tstring path = Strings::stringFormat(_T("%s\\TEST.LOG"), szPath );
-	bool bOpen = f.Open( path.c_str() );
-	f.Write("this is a test\r\n");
-	f.Close();
-	Logger::log_debug("COnSipDevice::OpenDevice filetest path=%s bOpen=%d", path.c_str(), bOpen );
+//	FileIO f;
+//	TCHAR szPath[MAX_PATH];
+//	SHGetFolderPath( NULL, CSIDL_COMMON_APPDATA, NULL, 0, szPath );
+//	tstring path = Strings::stringFormat(_T("%s\\TEST.LOG"), szPath );
+//	bool bOpen = f.Open( path.c_str() );
+//	f.Write("this is a test\r\n");
+//	f.Close();
+//	Logger::log_debug("COnSipDevice::OpenDevice filetest path=%s bOpen=%d", path.c_str(), bOpen );
 
 	// Cast the connection to a line
 	CTSPILineConnection* pLine = dynamic_cast<CTSPILineConnection*>(pConn);
